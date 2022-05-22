9d9c49f81136df74a98e2e8ce0258463333babde
yzy32 <yzyang32@gmail.com>
2022-05-22 16:38:02 +0800

    [UnitTest]createUser,getUserInfo,getFollowDetails

---
+++ test/set_up.js
@@ -0,0 +1,12 @@
+const app = require("../app");
+const chai = require("chai");
+
+const assert = chai.assert;
+const expect = chai.expect;
+// const requester = chai.request(app).keepOpen();
+
+module.exports = {
+  expect,
+  assert,
+  // requester,
+};
---
+++ test/user_test.js
@@ -0,0 +1,136 @@
+require("dotenv").config({ path: __dirname + "../.env" });
+const { assert, expect } = require("./set_up");
+const {
+  createUser,
+  getUserInfo,
+  getFollowDetails,
+} = require("../server/models/user_model");
+const bcrypt = require("bcrypt");
+const tsutsuError = require("../utils/error");
+const jwt = require("jsonwebtoken");
+
+describe("user", () => {
+  it("normal sign up", async () => {
+    const user = {
+      userId: "chai",
+      type: "native",
+      email: "chai@gmail.com",
+      password: "test",
+    };
+    const result = await createUser(
+      user.userId,
+      user.type,
+      user.email,
+      user.password
+    );
+    assert.hasAllKeys(
+      result,
+      ["user", "accessToken", "accessExpired", "error"],
+      "result should have keys: user, accessToken, accessExpired, error"
+    );
+    assert.typeOf(result.error, "null", "error should be null");
+  });
+
+  it("sign up with existing userId", async () => {
+    const user = {
+      userId: "chai",
+      type: "native",
+      email: "chai02@gmail.com",
+      password: "test",
+    };
+    const result = await createUser(
+      user.userId,
+      user.type,
+      user.email,
+      user.password
+    );
+    expect(result.error).haveOwnPropertyDescriptor("code", {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: 11000,
+    });
+    expect(result.error.keyPattern).haveOwnPropertyDescriptor("userId");
+  });
+
+  it("sign up with existing userId", async () => {
+    const user = {
+      userId: "chai02",
+      type: "native",
+      email: "chai@gmail.com",
+      password: "test",
+    };
+    const result = await createUser(
+      user.userId,
+      user.type,
+      user.email,
+      user.password
+    );
+    expect(result.error).haveOwnPropertyDescriptor("code", {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: 11000,
+    });
+    expect(result.error.keyPattern).haveOwnPropertyDescriptor("email");
+  });
+
+  it("normal sign in", async () => {
+    const user = { type: "native", email: "chai@gmail.com", password: "test" };
+    const result = await getUserInfo(user.type, user.email);
+    const decoded = await bcrypt.compare(user.password, result.password);
+    expect(decoded).to.be.true;
+  });
+
+  it("sign in with wrong password", async () => {
+    const user = { type: "native", email: "chai@gmail.com", password: "wrong" };
+    const result = await getUserInfo(user.type, user.email);
+    const decoded = await bcrypt.compare(user.password, result.password);
+    expect(decoded).to.be.false;
+  });
+
+  it("sign in with non-existing email", async () => {
+    const user = {
+      type: "native",
+      email: "chai_noExist@gmail.com",
+      password: "test",
+    };
+    const result = await getUserInfo(user.type, user.email);
+    expect(result).to.be.null;
+  });
+
+  it("get follower list", async () => {
+    const authorFollower = {
+      authorId: "yazhu",
+      followField: "follower",
+      page: 1,
+      followPageSize: 20,
+    };
+    const result = await getFollowDetails(
+      authorFollower.authorId,
+      authorFollower.followField,
+      authorFollower.page,
+      authorFollower.followPageSize
+    );
+    assert.hasAllKeys(result, ["followDetailList"]);
+  });
+
+  it("get non-existing author's follower list", async () => {
+    const authorFollower = {
+      authorId: "not-exist",
+      followField: "follower",
+      page: 1,
+      followPageSize: 20,
+    };
+    const err = new tsutsuError(404, "Author not found");
+    await expect(
+      getFollowDetails.bind(
+        null,
+        authorFollower.authorId,
+        authorFollower.followField,
+        authorFollower.page,
+        authorFollower.followPageSize
+      )
+    ).to.throw(err);
+  });
+});
